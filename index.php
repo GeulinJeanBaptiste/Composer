792f30aebd1f7d75fd9465fda0aca35cfddf6502
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2024-01-03 14:39:34 +0100

    création card

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \Colors\RandomColor;
 
+// dump(RandomColor::one());
+
+$faker = Faker\Factory::create('fr_FR');
+
+$faker->name() . '<br>';
+
+$faker->email() . '<br>';
+
+$faker->text() . '<br>';
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +28,42 @@ use \Colors\RandomColor;
     .colAI {
         color: <?= RandomColor::one() ?>;
     }
+
+    body {
+        background-color: whitesmoke;
+    }
+
+    .card {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0.2rem;
+    }
+
+    .card article {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        width: 25%;
+        margin: auto;
+        border: 2px solid black;
+    }
 </style>
 
 <body>
-    <h1 class="colAI">Ben Con</h1>
+    <section class="card">
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+            <article>
+                <h1 class="colAI"><?= ucfirst($faker->words(5, true)) ?></h1>
+                <p><img src="<?= $faker->imageUrl(360, 360, 'animals', true) ?>" alt=""></p>
+                <p><?= $faker->text() ?></p>
+                <p><?= $faker->name() ?></p>
+                <p><?= $faker->email() ?></p>
+                <p><?= $faker->date('d/m/Y') ?></p>
+                <p><?= $faker->time() ?></p>
+            </article>
+        <?php endfor ?>
+    </section>
 
 </body>
 
