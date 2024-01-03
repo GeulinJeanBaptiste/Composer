506ce3e531617528a051816fd2d2bc5d289986ba
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2024-01-03 13:41:28 +0100

    modif index

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use \Colors\RandomColor;
 
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Composer</title>
+</head>
+<style>
+    .colAI {
+        color: <?= RandomColor::one() ?>;
+    }
+</style>
 
+<body>
+    <h1 class="colAI">Ben Con</h1>
 
+</body>
+
+</html>
